ef8993e46067d663ff1fa315fba9034f0476ff72
pavex <pavex@ines.cz>
2018-07-26 11:38:03 +0200

    modal-window package rename

--- src/Navigator.js
+++ src/Navigator.js
@@ -4,7 +4,7 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import ModalWindow from 'react-ui-modal-window';
+import ModalWindow from 'pavex-react-ui-modal-window';
 
 
 export default class Navigator extends React.Component {
